b2686164d763462a3bcf2655329ec15eb21c8233
azozmohd38 <azozmohd38@gmail.com>
2026-09-07 12:05:20 +0400

    Add available vehicle query

--- src/main/java/com/example/logisticsnetwork/repository/VehicleRepository.java
+++ src/main/java/com/example/logisticsnetwork/repository/VehicleRepository.java
@@ -2,10 +2,15 @@ package com.example.logisticsnetwork.repository;
 
 import com.example.logisticsnetwork.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
     List<Vehicle> findAllByIsActiveTrue();
     Optional<Vehicle> findByIdAndIsActiveTrue(Long id);
+
+    @Query("select v from Vehicle v where v.isActive = true and lower(v.status) = 'available'")
+    List<Vehicle> findCurrentlyAvailable();
 }
