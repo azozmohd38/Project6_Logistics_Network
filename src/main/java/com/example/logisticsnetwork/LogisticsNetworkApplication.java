f9160d0d43694ba36e2355b306ec9ecb68ffeb08
azozmohd38 <azozmohd38@gmail.com>
2026-09-07 11:55:02 +0400

    Add Spring Boot application entry point

---
+++ src/main/java/com/example/logisticsnetwork/LogisticsNetworkApplication.java
@@ -0,0 +1,11 @@
+package com.example.logisticsnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsNetworkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticsNetworkApplication.class, args);
+    }
+}
