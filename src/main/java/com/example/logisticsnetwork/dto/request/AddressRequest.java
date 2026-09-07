86e9a491a5e89f58ff50ef9acac87c15b4eb3f3c
azozmohd38 <azozmohd38@gmail.com>
2026-09-07 12:11:24 +0400

    Accept optional service zone in address requests

--- src/main/java/com/example/logisticsnetwork/dto/request/AddressRequest.java
+++ src/main/java/com/example/logisticsnetwork/dto/request/AddressRequest.java
@@ -1,11 +1,17 @@
 package com.example.logisticsnetwork.dto.request;
+
 import jakarta.validation.constraints.*;
 import lombok.*;
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressRequest {
- @NotBlank @Size(max=180) private String street;
- @NotBlank @Size(max=100) private String city;
- @Size(max=20) private String postalCode;
- @NotBlank @Size(max=100) private String country;
- @NotNull @Positive private Long customerId;
+    @NotBlank @Size(max=180) private String street;
+    @NotBlank @Size(max=100) private String city;
+    @Size(max=20) private String postalCode;
+    @NotBlank @Size(max=100) private String country;
+    @NotNull @Positive private Long customerId;
+    @Positive private Long serviceZoneId;
 }
